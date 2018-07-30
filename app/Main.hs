228fe9cb1d5cbf543fec496fa5a59aa6f4d16426
Alexander Danilov <alexander.a.danilov@gmail.com>
2018-07-30 14:30:51 +0300

    Add documentation.

--- app/Main.hs
+++ app/Main.hs
@@ -7,11 +7,19 @@ import           Web.Browser        (openBrowser)
 port :: Int
 port = 9999
 
+-- | Задержка перез запуском браузера в мс
+delay :: Int
+delay = 3000000
+
+
 main :: IO ()
 main = do
+  -- порождаем дочерний процесс, который запустить браузер с url игры через несколько секунд
   forkOS $ do
-    threadDelay 3000000
-    r <- openBrowser $ "http://localhost:" ++ (show port)
-    print r
+    threadDelay delay
+    let url = "http://localhost:" ++ show port
+    _ <- openBrowser url
+    putStrLn $ "Open " ++ url
+  -- запускаем приложение
   startApp port
 
